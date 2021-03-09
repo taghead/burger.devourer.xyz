0d788062e45c097816119decf8ac9789b72377af
Andrew <taghead@taghead.xyz>
2021-03-10 02:18:32 +1100

    CU-34rw2w - Simple text page implemented

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass']
+})
+export class HeaderComponent implements OnInit {
+  title = "Taghead.xyz";
+  titleSubText = "Consume the borgor";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  getTitleSubText() {
+    return this.titleSubText;
+  }
+
+}
