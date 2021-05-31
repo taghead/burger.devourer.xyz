09bc3c025e425aba32aeeafd913594127f7cfd84
Andrew <taghead@taghead.xyz>
2021-05-31 18:52:44 +1000

    Video counter added

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -21,14 +21,17 @@ export class BodyComponent implements OnInit {
   }
 
   getVideo(){
-    let videos = ["j8H4pm9Q-Rw", "qlCGh33eIFQ", "ud-jsyDQBOY", "KcDVHkdrBlw", "6f8jqEmDFVo", "ujNf5HPbvkA",
-              "yPxfBpBgJ5U", "E-xhxS581Uc", "jXTp-tSUGZ8", "l-fs3cK9W5E", "J9Id5dDTYz8", "ruRVF1WM6Mg",
-              "1Ma4K2518NA", "doQljoFtBIs"];
+    let videos = ["j8H4pm9Q-Rw", "qlCGh33eIFQ", "ud-jsyDQBOY", "KcDVHkdrBlw", "6f8jqEmDFVo",
+                 "E-xhxS581Uc", "J9Id5dDTYz8", "1Ma4K2518NA", "doQljoFtBIs"];
 
     let url="https://www.youtube-nocookie.com/embed/";
-    let video=videos[Math.floor(Math.random() * videos.length)];
+    let randomNumber = Math.floor(Math.random() * videos.length)
+    let video=videos[randomNumber];
     let urlArgs = "?autoplay=1&t=4&loop=1&modestbranding=1&showinfo=0&rel=0&cc_load_policy=1&iv_load_policy=3&fs=0&loop=1&rel=0";
     let videoElement = document.getElementById("borgor-video");
     videoElement.setAttribute("src", url+video+urlArgs);
+
+    let videoCount = document.getElementById("video-count");
+    videoCount.innerHTML = `Currently consuming burger ${randomNumber} out of ${videos.length}`
   }
 }
