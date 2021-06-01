449009c58cc24ffedd6c216f9cbc4605775f2dc5
Andrew <taghead@taghead.xyz>
2021-06-01 10:59:20 +1000

    Removed gap between main and toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -30,7 +31,8 @@ import { BodyComponent } from './body/body.component';
     MatIconModule,
     MatButtonModule,
     MatExpansionModule,
-    MatGridListModule
+    MatGridListModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
