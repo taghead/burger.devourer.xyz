c2e8a7e46406a11c1b9356e52eed2cfe33bea6ed
Andrew <taghead@taghead.xyz>
2021-07-13 00:14:43 +1000

    Added environment variables to .gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-
+  supabase_url: 'https://udlsuchqmzwiaihnweeb.supabase.co',
+  supabase_key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNTk4NTI0MiwiZXhwIjoxOTQxNTYxMjQyfQ.WUsZHeekkwTkL7X5nBdAVrgmmPUXBucXHtfbrBET40M'
 };
