c384b4e189c8d6c1223191aa36841c4cf1d35ba4
Andrew <taghead@taghead.xyz>
2021-07-13 00:20:47 +1000

    I screwed up, fixing now

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabase_url: 'https://udlsuchqmzwiaihnweeb.supabase.co',
-  supabase_key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNTk4NTI0MiwiZXhwIjoxOTQxNTYxMjQyfQ.WUsZHeekkwTkL7X5nBdAVrgmmPUXBucXHtfbrBET40M'
+  supabase_url: 'Got a new url  rekt',
+  supabase_key: ':wave: Rerolled my api key'
 };
