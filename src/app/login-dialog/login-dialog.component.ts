fc88120bf0f4bac893f750f76b1c46a8d376543b
Andrew <s3666434@student.rmit.edu.au>
2021-07-16 05:15:53 +1000

    Added required services and forms for authentication

--- src/app/login-dialog/login-dialog.component.ts
+++ src/app/login-dialog/login-dialog.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { SupabaseService } from '../supabase.service'
+import { Provider } from '@supabase/supabase-js'
+import { Router } from '@angular/router'
+
+interface HelperText {
+  text: string
+  error: boolean
+}
 
 @Component({
   selector: 'app-login-dialog',
@@ -7,18 +15,55 @@ import {FormControl, Validators} from '@angular/forms';
   styleUrls: ['./login-dialog.component.sass'],
   encapsulation: ViewEncapsulation.None
 })
-export class LoginDialogComponent implements OnInit {
-  hide = true;
-  email = new FormControl('', [Validators.required, Validators.email]);
 
-  constructor() { }
-  ngOnInit(): void { }
+export class LoginDialogComponent {
+  authForm: FormGroup = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  })
 
-  getErrorMessage = () => {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
+  helperText: HelperText | undefined
+
+  constructor(private readonly supabase: SupabaseService, private readonly router: Router) {}
+
+  async forgotPassword(): Promise<void> {
+    const email = prompt('Please enter your email:')
+
+    let { error } = await this.supabase.resetPassword(email as string)
+    if (error) {
+      console.error('Error: ', error.message)
+    } else {
+      this.helperText = {
+        error: false,
+        text: 'Password recovery email has been sent.',
+      }
     }
+  }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+  async handleLogin(type: string): Promise<void> {
+    const { email, password } = this.authForm.value
+
+    const { user, error, session } =
+      type === 'LOGIN'
+        ? await this.supabase.signIn(email, password)
+        : await this.supabase.signUp(email, password)
+
+    if (user && type === 'LOGIN') {
+      await this.router.navigate(['/'])
+    } else if (error) {
+      this.helperText = { error: true, text: error.message }
+    } else if (user && !session && !error) {
+      this.helperText = {
+        error: false,
+        text: 'An email has been sent to you for verification!',
+      }
+    }
+  }
+
+  async handleOAuthLogin(provider: Provider): Promise<void> {
+    // You need to enable the third party auth you want in Authentication > Settings
+    // Read more on: https://supabase.io/docs/guides/auth#third-party-logins
+    let { error } = await this.supabase.signInWithProvider(provider)
+    if (error) console.error('Error: ', error.message)
   }
 }
